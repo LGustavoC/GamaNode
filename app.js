09169138532b80915ea67ac3c5eb05b38b86ceaf
LGustavoC <gugacristo90@gmail.com>
2021-08-23 23:46:33 -0300

    Criar, atualizar, selecionar e remover usuário salvando em JSON utilizando node.js por meio da url

--- app.js
+++ app.js
@@ -2,40 +2,67 @@
 const http = require('http');
 const url = require('url');
 const queryString = require('query-string');
+const fs = require('fs');
 
-// Definição de endereço / URL
+// Definicaoo de endereço / URL
 const hostname = '127.0.0.1'; // localhost
 const port = 3000;
 
-// Implementação da regra de negócio
+// Implementação da regra de negocio
 const server = http.createServer((req, res) => {
 
-    // Pegar a pergunta na url
-    const params = queryString.parse(url.parse(req.url, true).search);
-    // console.log(params);
+  let resposta;
+  const urlparse = url.parse(req.url, true);
 
-    // Verificar a pergunta e escolher uma resposta
-    let resposta;
+  // Receber informacoes do usuario
+  const params = queryString.parse(urlparse.search);
 
-    if(params.pergunta == 'melhor-filme'){
-      resposta = 'A procura da felicidade';
-      // console.log('sempre ao seu lado');
-    } 
-    else if(params.pergunta == 'melhor-tecnologia-backend'){
-      resposta = 'Node.js!';
-      // console.log('Node.js!')
-    }else {
-      reposta = 'Não sei, desculpe :(';
-      // console.log('não sei, desculpe :(')
-    }
-    // Retornar a resposta escolhida
-  
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(resposta);
+  // Criar usuario - Atualizar usuario
+  if (urlparse.pathname == '/criar-atualizar-usuario') {
+    // Salvar informacoes
+    fs.writeFile(`users/${params.id}.txt`, JSON.stringify(params), function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+      
+      resposta = 'Usuario criado/atualizado com sucesso'
+
+      res.statusCode = 201;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(resposta);
+    });
+  }
+  // Selecionar usuario
+  else if (urlparse.pathname == '/selecionar-usuario') {
+    fs.readFile(`users/${params.id}.txt`, function(err, data) {
+      resposta = data;
+      console.log('Selecionado!');
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(resposta);
+    });
+  }
+  // Remover usuario
+  else if (urlparse.pathname == '/remover-usuario') {
+    fs.unlink(`users/${params.id}.txt`, function (err) {
+      console.log('File deleted!');
+
+      resposta = err ? "Usuario nao encontrado" : "Usuario removido";
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(resposta);
+    });
+  }
 })
 
-// Execução
+
+
+// Execucao
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
+
+// http://localhost:3000/criar-atualizar-usuario?nome=Guga&idade=20&id=1
+// http://localhost:3000/selecionar-usuario?nome=Guga&idade=20&id=1
+// http://localhost:3000/remover-usuario?id=1
